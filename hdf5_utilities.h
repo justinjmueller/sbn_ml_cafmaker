3967c9b907d6d47c7bd9d2fc7e158001d0bebacb
justinjmueller <justin.mueller@colostate.edu>
2023-11-14 10:37:10 -0600

    Restructure StandardRecord to reflect bidirectional matching

---
+++ hdf5_utilities.h
@@ -0,0 +1,74 @@
+#include "H5Cpp.h"
+#include "sbn_h5_classes.h"
+#include "record_fillers.h"
+#include "sbnanaobj/StandardRecord/StandardRecord.h"
+
+size_t get_nevents(H5::DataSpace &dsp)
+{
+  hsize_t dims_max = dsp.getSimpleExtentNdims();
+  auto dims = new hsize_t[dims_max];
+  dsp.getSimpleExtentDims(dims);
+  size_t nevents(dims[dims_max - 1]);
+  delete[] dims;
+  return nevents;
+}
+
+std::vector<dlp::types::Event> get_events(H5::H5File &file, int64_t index)
+{
+  H5::DataSet dataset(file.openDataSet("events"));
+  H5::DataSpace dsp(dataset.getSpace());
+
+  std::vector<dlp::types::Event> evt;
+  if(index < 0)
+  {
+    evt.resize(get_nevents(dsp));
+    H5::CompType ctype(dlp::types::BuildCompType<dlp::types::Event>());
+    dataset.read(evt.data(), ctype, H5::DataSpace::ALL, H5::DataSpace::ALL);
+  }
+  else
+  {
+    evt.resize(1);
+    std::vector<hsize_t> start(1, index);
+    std::vector<hsize_t> count(1, 1);
+    dsp.selectHyperslab(H5S_SELECT_SET, count.data(), start.data());
+
+    H5::DataSpace memspace(H5S_SIMPLE);
+    std::vector<hsize_t> dims(dsp.getSimpleExtentNdims());
+    std::vector<hsize_t> dimsMax(dsp.getSimpleExtentNdims(), H5S_UNLIMITED);
+    dsp.getSimpleExtentDims(dims.data());
+    memspace.setExtentSimple(dsp.getSimpleExtentNdims(), dims.data(), dimsMax.data());
+    start[0] = 0;
+    count[0] = 1;
+    memspace.selectHyperslab(H5S_SELECT_SET, count.data(), start.data());
+  
+    H5::CompType ctype(dlp::types::BuildCompType<dlp::types::Event>());
+    dataset.read(evt.data(), ctype, memspace, dsp);
+  }
+
+  return evt;
+}
+
+template <class T>
+std::vector<T> get_product(H5::H5File &file, dlp::types::Event &evt)
+{
+  H5::DataSet ds_ref(file.openDataSet("events"));
+  
+  const void *buff_ref_const(&(evt.GetRef<T>()));
+  ds_ref.dereference(file, buff_ref_const, H5R_DATASET_REGION);
+  void *buff_ref(&(const_cast<hdset_reg_ref_t&>(evt.GetRef<T>())));
+  H5::DataSpace ref_region = file.getRegion(buff_ref);
+
+  H5::DataSpace memspace(H5S_SIMPLE);
+  std::vector<hsize_t> dims(ref_region.getSimpleExtentNdims());
+  std::vector<hsize_t> dims_max(ref_region.getSimpleExtentNdims(), H5S_UNLIMITED);
+  ref_region.getSimpleExtentDims(dims.data());
+  memspace.setExtentSimple(1, dims.data(), dims_max.data());
+  std::vector<hsize_t> start(1, 0);
+  std::vector<hsize_t> count(1, static_cast<hsize_t>(ref_region.getSelectNpoints()));
+  memspace.selectHyperslab(H5S_SELECT_SET, count.data(), start.data());
+
+  std::vector<T> data_product(static_cast<hsize_t>(ref_region.getSelectNpoints()));
+  H5::CompType ctype(dlp::types::BuildCompType<T>());
+  ds_ref.read(data_product.data(), ctype, memspace, ref_region);
+  return data_product;
+}
--- main.cc
+++ main.cc
@@ -4,27 +4,16 @@
 #include "H5Cpp.h"
 #include "sbn_h5_classes.h"
 #include "record_fillers.h"
+#include "hdf5_utilities.h"
 #include "sbnanaobj/StandardRecord/StandardRecord.h"
 #include "sbnanaobj/StandardRecord/SRInteractionDLP.h"
+#include "sbnanaobj/StandardRecord/SRInteractionTruthDLP.h"
+#include "sbnanaobj/StandardRecord/SRParticleDLP.h"
+#include "sbnanaobj/StandardRecord/SRParticleTruthDLP.h"
+#include "sbnanaobj/StandardRecord/SRIMatchDLP.h"
+#include "sbnanaobj/StandardRecord/SRPMatchDLP.h"
 #include "TFile.h"
 #include "TTree.h"
-
-std::vector<dlp::types::Event> get_events(H5::H5File &file, int64_t index);
-
-template <class T>
-std::vector<T> get_product(H5::H5File &file, dlp::types::Event &evt);
-
-std::ostream& operator<<(std::ostream& os, const dlp::types::Particle& p);
-
-size_t get_nevents(H5::DataSpace &dsp)
-{
-  hsize_t dims_max = dsp.getSimpleExtentNdims();
-  auto dims = new hsize_t[dims_max];
-  dsp.getSimpleExtentDims(dims);
-  size_t nevents(dims[dims_max - 1]);
-  delete[] dims;
-  return nevents;
-}
 
 int main(int argc, char const *argv[])
 {
@@ -37,7 +26,7 @@ int main(int argc, char const *argv[])
   H5::H5File file(argv[1], H5F_ACC_RDONLY);
   std::cout << "Opened file: " << argv[1] << "\n" << std::endl;
 
-  TFile caf("dlp.caf.root", "recreate");
+  TFile caf("dlp_revised.caf.root", "recreate");
   TTree rec_tree("dlp", "Event tree for ML reconstruction");
   caf::StandardRecord *rec;
   rec_tree.Branch("rec", &rec);
@@ -45,18 +34,81 @@ int main(int argc, char const *argv[])
   std::vector<dlp::types::Event> events(get_events(file, -1));
   for(dlp::types::Event &evt : events)
   {
-    caf::StandardRecord sr;
-    std::vector<dlp::types::Interaction> interactions(get_product<dlp::types::Interaction>(file, evt));
-    std::vector<dlp::types::Particle> particles(get_product<dlp::types::Particle>(file, evt));
-    for(dlp::types::Interaction &i : interactions)
-      sr.dlp.push_back(fill_interaction(i, particles));
-    sr.ndlp = sr.dlp.size();
-
-    std::vector<dlp::types::TruthInteraction> true_interactions(get_product<dlp::types::TruthInteraction>(file, evt));
+    std::vector<dlp::types::Particle> reco_particles(get_product<dlp::types::Particle>(file, evt));
     std::vector<dlp::types::TruthParticle> true_particles(get_product<dlp::types::TruthParticle>(file, evt));
+
+    std::vector<caf::SRParticleDLP> caf_reco_particles;
+    for(dlp::types::Particle &p : reco_particles)
+      caf_reco_particles.push_back(fill_particle(p));
+
+    std::vector<caf::SRParticleTruthDLP> caf_true_particles;
+    for(dlp::types::TruthParticle &p : true_particles)
+      caf_true_particles.push_back(fill_truth_particle(p));
+
+    std::vector<caf::SRPMatchDLP> caf_pmatch_reco;
+    for(caf::SRParticleDLP &p : caf_reco_particles)
+    {
+      caf::SRPMatchDLP pmatch;
+      pmatch.x = p;
+      if(p.match.size() > 0)
+      {
+	pmatch.y = caf_true_particles.at(p.match[0]);
+	pmatch.iou = p.match_overlap[0];
+      }
+      caf_pmatch_reco.push_back(pmatch);
+    }
+
+    std::vector<caf::SRPMatchTruthDLP> caf_pmatch_true;
+    for(caf::SRParticleTruthDLP &p : caf_true_particles)
+    {
+      caf::SRPMatchTruthDLP pmatch;
+      pmatch.x = p;
+      if(p.match.size() > 0)
+      {
+	pmatch.y = caf_reco_particles.at(p.match[0]);
+	pmatch.iou = p.match_overlap[0];
+      }
+      caf_pmatch_true.push_back(pmatch);
+    }
+
+    std::vector<dlp::types::Interaction> reco_interactions(get_product<dlp::types::Interaction>(file, evt));
+    std::vector<dlp::types::TruthInteraction> true_interactions(get_product<dlp::types::TruthInteraction>(file, evt));
+
+    std::vector<caf::SRInteractionDLP> caf_reco_interactions;
+    for(dlp::types::Interaction &i : reco_interactions)
+      caf_reco_interactions.push_back(fill_interaction(i, caf_pmatch_reco));
+
+    std::vector<caf::SRInteractionTruthDLP> caf_true_interactions;
     for(dlp::types::TruthInteraction &i : true_interactions)
-      sr.dlp_true.push_back(fill_truth_interaction(i, true_particles));
-    sr.ndlp_true = sr.dlp_true.size();
+      caf_true_interactions.push_back(fill_truth_interaction(i, caf_pmatch_true));
+
+    caf::StandardRecord sr;
+
+    for(caf::SRInteractionDLP &i : caf_reco_interactions)
+    {
+      caf::SRIMatchDLP imatch;
+      imatch.x = i;
+      if(i.match.size() > 0)
+      {
+	imatch.y = caf_true_interactions.at(i.match[0]);
+	imatch.iou = i.match_overlap[0];
+      }
+      ++sr.ndlp;
+      sr.dlp.push_back(imatch);
+    }
+
+    for(caf::SRInteractionTruthDLP &i : caf_true_interactions)
+    {
+      caf::SRIMatchTruthDLP imatch;
+      imatch.x = i;
+      if(i.match.size() > 0)
+      {
+	imatch.y = caf_reco_interactions.at(i.match[0]);
+	imatch.iou = i.match_overlap[0];
+      }
+      ++sr.ndlp_true;
+      sr.dlp_true.push_back(imatch);
+    }
   
     rec = &sr;
     rec_tree.Fill();
@@ -69,75 +121,3 @@ int main(int argc, char const *argv[])
   return 0;
 }
 
-std::vector<dlp::types::Event> get_events(H5::H5File &file, int64_t index)
-{
-  H5::DataSet dataset(file.openDataSet("events"));
-  H5::DataSpace dsp(dataset.getSpace());
-
-  std::vector<dlp::types::Event> evt;
-  if(index < 0)
-  {
-    evt.resize(get_nevents(dsp));
-    H5::CompType ctype(dlp::types::BuildCompType<dlp::types::Event>());
-    dataset.read(evt.data(), ctype, H5::DataSpace::ALL, H5::DataSpace::ALL);
-  }
-  else
-  {
-    evt.resize(1);
-    std::vector<hsize_t> start(1, index);
-    std::vector<hsize_t> count(1, 1);
-    dsp.selectHyperslab(H5S_SELECT_SET, count.data(), start.data());
-
-    H5::DataSpace memspace(H5S_SIMPLE);
-    std::vector<hsize_t> dims(dsp.getSimpleExtentNdims());
-    std::vector<hsize_t> dimsMax(dsp.getSimpleExtentNdims(), H5S_UNLIMITED);
-    dsp.getSimpleExtentDims(dims.data());
-    memspace.setExtentSimple(dsp.getSimpleExtentNdims(), dims.data(), dimsMax.data());
-    start[0] = 0;
-    count[0] = 1;
-    memspace.selectHyperslab(H5S_SELECT_SET, count.data(), start.data());
-  
-    H5::CompType ctype(dlp::types::BuildCompType<dlp::types::Event>());
-    dataset.read(evt.data(), ctype, memspace, dsp);
-  }
-
-  return evt;
-}
-
-template <class T>
-std::vector<T> get_product(H5::H5File &file, dlp::types::Event &evt)
-{
-  H5::DataSet ds_ref(file.openDataSet("events"));
-  
-  const void *buff_ref_const(&(evt.GetRef<T>()));
-  ds_ref.dereference(file, buff_ref_const, H5R_DATASET_REGION);
-  void *buff_ref(&(const_cast<hdset_reg_ref_t&>(evt.GetRef<T>())));
-  H5::DataSpace ref_region = file.getRegion(buff_ref);
-
-  H5::DataSpace memspace(H5S_SIMPLE);
-  std::vector<hsize_t> dims(ref_region.getSimpleExtentNdims());
-  std::vector<hsize_t> dims_max(ref_region.getSimpleExtentNdims(), H5S_UNLIMITED);
-  ref_region.getSimpleExtentDims(dims.data());
-  memspace.setExtentSimple(1, dims.data(), dims_max.data());
-  std::vector<hsize_t> start(1, 0);
-  std::vector<hsize_t> count(1, static_cast<hsize_t>(ref_region.getSelectNpoints()));
-  memspace.selectHyperslab(H5S_SELECT_SET, count.data(), start.data());
-
-  std::vector<T> data_product(static_cast<hsize_t>(ref_region.getSelectNpoints()));
-  H5::CompType ctype(dlp::types::BuildCompType<T>());
-  ds_ref.read(data_product.data(), ctype, memspace, ref_region);
-  return data_product;
-}
-
-std::ostream& operator<<(std::ostream& os, const dlp::types::Particle& p)
-{
-  os << "Particle [image_id=" << p.image_id 
-     << ", interaction_id=" << p.interaction_id
-     << ", id=" << p.id << "]\n"
-     << "\tpid: " << int(p.pid) << "\n"
-     << "\tprimary: " << p.is_primary << "\n"
-     << "\tcontained: " << p.is_contained << "\n"
-     << std::endl;
-  
-  return os;
-}
