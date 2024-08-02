9b7e159b48862092f393c39f288cb24606604a7e
justinjmueller <justin.mueller@colostate.edu>
2024-08-02 11:34:08 -0500

    Add comments to test_hdf5 source file

--- src/test_hdf5.cc
+++ src/test_hdf5.cc
@@ -6,14 +6,24 @@
 #include <iostream>
 #include <vector>
 #include "H5Cpp.h"
-//#include "/exp/icarus/app/users/mueller/sbn_ml_cafmaker_spineworkshop2024/sbn_ml_cafmaker/test_classes.h"
-//#include "/exp/icarus/app/users/mueller/sbn_ml_cafmaker_spineworkshop2024/sbn_ml_cafmaker/hdf5_utilities.h"
 #include "products.h"
 #include "event.h"
-//#include "runinfo.h"
 
+/**
+ * @brief A basic test program for reading in the H5 files produced by the
+ * SPINE reconstruction code.
+ * @param argc The number of command line arguments.
+ * @param argv The command line arguments. The first argument should be the
+ * name of the input file. The second argument is optional and specifies the
+ * event number to read in.
+ * @return 0 if the program completes successfully.
+*/
 int main(int argc, char const * argv[])
 {
+    /**
+     * @brief Check the arguments, verify a file name is provided, and set a
+     * default event number in case it is not specified.
+    */
     size_t event_number(0);
     if(argc < 2)
     {
@@ -22,35 +32,46 @@ int main(int argc, char const * argv[])
     }
     else if(argc == 3) event_number = std::stoi(argv[2]);
 
-    // Open the test input file
+    /**
+     * @brief Open the test input file.
+    */
     std::string input_file(argv[1]);
     std::cout << "Reading in test file: " << input_file << std::endl;
     H5::H5File file(input_file, H5F_ACC_RDONLY);
     std::cout << "Opened test file: " << input_file << std::endl;
     
-    // Get the number of events in the file.
+    /**
+     * @brief Get the events from the file and print out the number of events.
+    */
     std::vector<dlp::types::Event> events(get_all_events(file));
     std::vector<dlp::types::RunInfo> run_info(get_product<dlp::types::RunInfo>(file, events[event_number]));
     std::cout << "Number of events in file: " << events.size() << std::endl;
     
-    //std::cout << events[0].index << std::endl;
-    //std::cout << events[14].index << std::endl;
+    /**
+     * @brief Print out the run, subrun, and event numbers for the specified
+     * event. This validates that the RunInfo product is being read in
+     * correctly.
+    */
     std::cout << "Run: " << run_info[0].run << std::endl;
     std::cout << "Subrun: " << run_info[0].subrun << std::endl;
     std::cout << "Event: " << run_info[0].event << std::endl;
 
+    /**
+     * @brief Get the reco and truth products from the file and print out the
+     * number of reco and truth interactions and particles.
+    */
     std::vector<dlp::types::RecoInteraction> reco_interactions(get_product<dlp::types::RecoInteraction>(file, events[event_number]));
     std::cout << "Number of reco interactions: " << reco_interactions.size() << std::endl;
-
     std::vector<dlp::types::RecoParticle> reco_particles(get_product<dlp::types::RecoParticle>(file, events[event_number]));
     std::cout << "Number of reco particles: " << reco_particles.size() << std::endl;
-
     std::vector<dlp::types::TruthInteraction> truth_interactions(get_product<dlp::types::TruthInteraction>(file, events[event_number]));
     std::cout << "Number of truth interactions: " << truth_interactions.size() << std::endl;
-
     std::vector<dlp::types::TruthParticle> truth_particles(get_product<dlp::types::TruthParticle>(file, events[event_number]));
     std::cout << "Number of truth particles: " << truth_particles.size() << std::endl;
 
+    /**
+     * @brief Close the input file.
+    */
     file.close();
 
     return 0;
