18a5f9394e365f302a451e35959f09ae56f298f4
justinjmueller <justin.mueller@colostate.edu>
2024-11-13 16:07:37 -0600

    Add tool for printing dataset schema

---
+++ tools/print_attributes.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+import h5py
+
+def main(filename, dataset=None):
+    type_map = {'|u1': 'bool', '<i4': 'int32', '<i8': 'int64', '<f4': 'float', '<f8': 'double'}
+    with h5py.File(filename, 'r') as f:
+        if dataset is None:
+            print('No dataset specified. Printing existing datasets:')
+            for key in f.keys():
+                print(key)
+        else:
+            res = {}
+            for i in f[dataset].dtype.descr:
+                if type(i[1]) == str and i[1] in type_map.keys():
+                    res[i[0]] = type_map[i[1]]
+                    if len(i) > 2:
+                        res[i[0]] += f' {i[2]}'
+                else:
+                    res[i[0]] = i[1]
+            for key in sorted(res):
+                print(f'{key:<20}: {res[key]}')
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Print attributes of an HDF5 file or dataset')
+    parser.add_argument('--filename', help='HDF5 file')
+    parser.add_argument('--dataset', help='Dataset in HDF5 file')
+    args = parser.parse_args()
+    main(args.filename, args.dataset)
